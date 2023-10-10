7b0f81658ba0c9ba0ffb759319d4b267662d3db2
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-10-10 12:44:36 -0300

    metadata

--- src/app/[user]/page.jsx
+++ src/app/[user]/page.jsx
@@ -5,9 +5,10 @@ import CopiarPortapapeles from "@/components/copiar-portapapeles";
 import CopiarURL from "@/components/copiar-url";
 import UserCard from "@/components/user-card";
 export async function generateMetadata({ params: { user } }) {
-  const {name} = await getUser(user);
+  const {name, bio} = await getUser(user);
   return {
     title: "GitHub Finder | "+ name,
+    description: bio
   };
 }
 export default async function page({ params: { user } }) {
